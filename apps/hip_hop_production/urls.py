950550b1f2ae39203ae53309d765fdf8087ea8a6
Matt <matthewgstillman@gmail.com>
2018-08-07 20:49:10 -0700

    Added Search Function

--- apps/hip_hop_production/urls.py
+++ apps/hip_hop_production/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^form/$', views.form, name="form"),
+    url(r'^murda_beatz$', views.murda_beatz, name="murda_beatz"),
+    url(r'^result/$', views.result, name="result"),
 ]
--- apps/hip_hop_production/views.py
+++ apps/hip_hop_production/views.py
@@ -4,6 +4,68 @@ import wikipedia
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        producer = request.POST['producer']
+        request.session['producer'] = producer
+        context = {
+            'producer': producer,
+        }
+        return render(request, 'hip_hop_production/index.html', context)
+    else:
+        context = {
+        }
+        return render(request, 'hip_hop_production/index.html', context)
+
+def form(request):
+    if request.method == 'POST':
+        producer = request.POST['producer']
+        request.session['producer'] = producer
+        wiki_page = wikipedia.WikipediaPage(title=producer)
+        wiki_page_content = wiki_page.content
+        wiki_page_html = wiki_page.html()
+        print ("Wiki Page Content: " + wiki_page_content)
+        wiki_page_search = wikipedia.search(producer)
+        wiki_page = wikipedia.page(producer)
+        print("Wiki Page: " + str(wiki_page))
+        wiki_page_id = wiki_page.pageid
+        print("Page ID: " + str(wiki_page_id))
+        wiki_page_title = wiki_page.title
+        print("Title: " + str(wiki_page_title))
+        wiki_page_categories = wiki_page.categories
+        print("Categories: " + str(wiki_page_categories))
+        wiki_page_summary = wiki_page.summary
+        print("Summary: " + str(wiki_page_summary))
+        wiki_page_images = wiki_page.images
+        print("Images: " + str(wiki_page_images))
+        wiki_page_links = wiki_page.links
+        print("Links: " + str(wiki_page_links)) 
+        wiki_page_sections = wiki_page.sections
+        print("Sections: " + str(wiki_page_sections))
+        print("Producer: " + str(producer))
+        print("Wiki Page: " + str(wiki_page))
+        context = {
+            'producer': producer,
+            'wiki_page': wiki_page,
+            'wiki_page_content': wiki_page_content,
+            'wiki_page_html': wiki_page_html,
+            'wiki_page_search': wiki_page_search,
+            'wiki_page_id': wiki_page_id,
+            'wiki_page_title': wiki_page_title,
+            'wiki_page_categories': wiki_page_categories,
+            'wiki_page_summary': wiki_page_summary,
+            'wiki_page_images': wiki_page_images,
+            'wiki_page_links': wiki_page_links,
+            'wiki_page_sections': wiki_page_sections,
+
+        }
+        return render(request, 'hip_hop_production/result.html', context)
+    else:
+        context = {
+        }
+        return render(request, 'hip_hop_production/index.html', context)
+       
+
+def murda_beatz(request):
     url = ("http://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&titles=Murda_Beatz")
     response = requests.get(url)
     index = response.json()
@@ -32,6 +94,7 @@ def index(request):
     print("Response: " + str(response))
     print("Index: " + str(index))
     print("Real Page: " + str(real_page))
+    murda_beatz_wiki_images = murda_beatz_page.images
     context = {
         'response': response,
         'index': index,
@@ -43,7 +106,12 @@ def index(request):
         'murda_beatz_page': murda_beatz_page,
         'murda_beatz_sections': murda_beatz_sections,
         'murda_beatz_title': murda_beatz_title,
+        'murda_beatz_wiki_images': murda_beatz_wiki_images,
         'real_page_content': real_page_content,
         'real_page_html': real_page_html,
     }
-    return render(request, 'hip_hop_production/index.html', context)
+    return render(request, 'hip_hop_production/murda_beatz.html', context)
+
+
+def result(request):
+    return render(request, 'hip_hop_production/result.html')
